01e59cad023dc8f3f3a33f1f6916ebfd71efa4a0
Rinzan <nprinzan@gmail.com>
2025-08-07 19:34:02 +0530

    init

---
+++ Server/accounts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+
+# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('salesperson', 'Salesperson'),
+        ('accountant', 'Accountant'),
+    )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='salesperson')
