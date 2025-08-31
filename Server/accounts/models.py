177ce3ba6901c43d433348104c49e5521205dcc8
Rinzan <nprinzan@gmail.com>
2025-08-31 09:48:27 +0530

    accounts

--- Server/accounts/models.py
+++ Server/accounts/models.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import AbstractUser
 import uuid
 
 # Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
 class User(AbstractUser):
     ROLE_CHOICES = (
         ('admin', 'Admin'),
@@ -12,3 +20,4 @@ class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='salesperson')
     blocked = models.BooleanField(default=False)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='users', null = True, blank = True)
--- Server/accounts/serializers.py
+++ Server/accounts/serializers.py
@@ -9,13 +9,14 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model  = User
-        fields = ("id", "email", "password", "role")
+        fields = ("id", "email", "password", "role","company")
 
     def create(self, validated_data):
         user = User.objects.create_user(
             email=validated_data["email"],
-            username=validated_data["email"],  # Assuming email is used as username
+            username=validated_data["email"],  
             password=validated_data["password"],
+             company=validated_data["company"],
             role=validated_data.get("role", "Salesperson"),
         )
         return user
@@ -29,11 +30,12 @@ class LoginSerializer(serializers.Serializer):
 class SalesPersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("id", "email", "role")
-
+        fields = ("id", "email", "role", "company")
 
 class UserSerializer(serializers.ModelSerializer):
-    date = serializers.DateTimeField(source="date_joined", read_only=True,format="%d-%m-%Y %H:%M")
+    date = serializers.DateTimeField(source="date_joined", read_only=True, format="%d-%m-%Y %H:%M")
+    company_name = serializers.CharField(source="company.name", read_only=True)
+
     class Meta:
         model = User
-        fields = ("email", "role","blocked","date","id")
+        fields = ("email", "role", "blocked", "date", "id", "company_name")
